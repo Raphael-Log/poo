7c0ef61fb8b0c464b8531b38dbf99b9cbe2f07d1
RaphaelGab <aza.gabardo@gmail.com>
2023-10-19 20:57:27 -0300

    Alterado classe Autor

--- teste.py
+++ teste.py
@@ -1,2 +1,5 @@
 class Autor:
-    pass
+    def __init__(self, nome, ano_nascimento, endereco):
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+        self. _endereco = endereco
