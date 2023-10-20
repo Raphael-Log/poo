272133918fa3cc08eacbf0ce7d6eab29590694ff
RaphaelGab <aza.gabardo@gmail.com>
2023-10-19 21:12:29 -0300

    Adicionado calculo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from autor import Autor
 
 def main():
-    autor = Autor ('Antonio Neto', 1982, 'Rua Tal')
+    autor1 = Autor ('Antonio Neto', 1982, 'Rua Tal')
+    autor2 = Autor ('Carlos Braga', 1992, 'Rua Total')
+    print(autor1._nome)
+    print(autor2.calcula_idade(2023))
 
 if __name__ == "_main_":
     main()
