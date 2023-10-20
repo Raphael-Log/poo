8d4a9b149ee4f1f10ed7b967337b84272ba755f7
RaphaelGab <aza.gabardo@gmail.com>
2023-10-19 21:00:01 -0300

    Adicionado classe Livro

--- teste.py
+++ teste.py
@@ -3,3 +3,10 @@ class Autor:
         self._nome = nome
         self._ano_nascimento = ano_nascimento
         self. _endereco = endereco
+
+class Livro:
+     def __init__(self, titulo, ano, valor, autor):
+         self._titulo = titulo
+         self._ano = ano
+         self._valor = valor
+         self._autor = Autor
